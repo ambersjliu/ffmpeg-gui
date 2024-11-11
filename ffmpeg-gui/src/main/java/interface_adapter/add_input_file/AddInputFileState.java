21722ea3608bb098a1370669a2655ebbe08a6cac
ambersjliu <90348951+ambersjliu@users.noreply.github.com>
2024-11-11 16:18:30 -0500

    Added State and ViewModel

--- ffmpeg-gui/src/main/java/interface_adapter/add_input_file/AddInputFileState.java
+++ ffmpeg-gui/src/main/java/interface_adapter/add_input_file/AddInputFileState.java
@@ -1,4 +1,11 @@
 package interface_adapter.add_input_file;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class AddInputFileState {
+    private String filePath = "";
+    private String fileError;
 }
