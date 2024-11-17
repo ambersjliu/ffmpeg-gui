dda45684577687b5f3f416e2c1eefb79b68eaab4
notming11 <yinming.chan@gmail.com>
2024-11-17 00:08:42 -0500

    add init in appbuilder

--- ffmpeg-gui/src/main/java/app/App.java
+++ ffmpeg-gui/src/main/java/app/App.java
@@ -1,7 +1,9 @@
 package app;
 
 import interface_adapter.add_input_file.AddInputFileViewModel;
+import interface_adapter.get_paths_and_init.GetInputPathsAndInitViewModel;
 import view.AddInputFileView;
+import view.GetInputPathsAndInitView;
 
 import javax.swing.*;
 import java.awt.*;
@@ -10,13 +12,16 @@ import java.io.IOException;
 public class App {
     public static void main(String[] args) throws IOException {
         final AppBuilder appBuilder = new AppBuilder()
+                .addGetPathsAndInitView()
                 .addGetPathsAndInitUseCase();
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JPanel panel = new JPanel(new BorderLayout());
-        AddInputFileViewModel addInputFileViewModel = new AddInputFileViewModel();
-        AddInputFileView addInputFileView = new AddInputFileView(addInputFileViewModel);
-        panel.add(addInputFileView, BorderLayout.CENTER);
+        GetInputPathsAndInitViewModel getInputPathsAndInitViewModel = new GetInputPathsAndInitViewModel();
+        GetInputPathsAndInitView getInputPathsAndInitView = new GetInputPathsAndInitView(getInputPathsAndInitViewModel);
+//        AddInputFileViewModel addInputFileViewModel = new AddInputFileViewModel();
+//        AddInputFileView addInputFileView = new AddInputFileView(addInputFileViewModel);
+        panel.add(getInputPathsAndInitView, BorderLayout.CENTER);
         frame.add(panel, BorderLayout.CENTER);
         frame.pack();
         frame.setLocationRelativeTo(null);
--- ffmpeg-gui/src/main/java/app/AppBuilder.java
+++ ffmpeg-gui/src/main/java/app/AppBuilder.java
@@ -8,30 +8,56 @@ import interface_adapter.get_paths_and_init.GetInputPathsAndInitViewModel;
 import use_case.get_paths_and_init.GetPathsAndInitInputBoundary;
 import use_case.get_paths_and_init.GetPathsAndInitInteractor;
 import use_case.get_paths_and_init.GetPathsAndInitOutputBoundary;
+import view.AddInputFileView;
+import view.GetInputPathsAndInitView;
+
+import javax.swing.*;
+import java.awt.*;
 
 public class AppBuilder {
+    private final JPanel cardPanel = new JPanel();
+    private final CardLayout cardLayout = new CardLayout();
     // This is our "DAO"
     private final FFmpegService ffmpegService = new FFmpegService();
 
     private final ViewManagerModel viewManagerModel = new ViewManagerModel();
 
     private AddInputFileViewModel addInputFileViewModel;
+    private AddInputFileView addInputFileView;
+    private GetInputPathsAndInitView getInputPathsAndInitView;
     private GetInputPathsAndInitViewModel getInputPathsAndInitViewModel;
 
     public AppBuilder(){
+        cardPanel.setLayout(cardLayout);
+    }
 
+    public AppBuilder addGetPathsAndInitView(){
+        getInputPathsAndInitViewModel = new GetInputPathsAndInitViewModel();
+        getInputPathsAndInitView = new GetInputPathsAndInitView(getInputPathsAndInitViewModel);
+        cardPanel.add(getInputPathsAndInitView, getInputPathsAndInitView.getViewName());
+        return this;
     }
 
     public AppBuilder addGetPathsAndInitUseCase(){
-        // todo: incomplete
         final GetPathsAndInitOutputBoundary getPathsAndInitOutputBoundary =
                 new GetInputPathsAndInitPresenter(viewManagerModel, addInputFileViewModel, getInputPathsAndInitViewModel); // should have viewmodel as param
 
         final GetPathsAndInitInputBoundary getPathsAndInitInputBoundary =
                 new GetPathsAndInitInteractor(getPathsAndInitOutputBoundary, ffmpegService);
-        // Todo: controller
-        // todo: set controller of the getpathsinit view
+
+        cardPanel.add(getInputPathsAndInitView, getInputPathsAndInitView.getViewName());
         return this;
     }
 
+    public JFrame build(){
+        final JFrame application = new JFrame("Setting ffmpeg file path");
+        application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        application.add(cardPanel);
+
+        viewManagerModel.setState(getInputPathsAndInitView.getViewName());
+        viewManagerModel.firePropertyChanged();
+
+        return application;
+    }
 }
