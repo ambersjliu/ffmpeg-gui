1b1e483a6dbd6466c6de573c4f1b82481784bfb0
Jack <jackkh.cheng@mail.utoronto.ca>
2024-11-22 23:04:13 -0500

    Preliminary implementation of convertVideo method

--- ffmpeg-gui/src/main/java/data_access/FFmpegService.java
+++ ffmpeg-gui/src/main/java/data_access/FFmpegService.java
@@ -7,7 +7,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import net.bramp.ffmpeg.FFmpeg;
+import net.bramp.ffmpeg.FFmpegExecutor;
 import net.bramp.ffmpeg.FFprobe;
+import net.bramp.ffmpeg.builder.FFmpegBuilder;
 import net.bramp.ffmpeg.probe.FFmpegProbeResult;
 
 import java.io.IOException;
@@ -38,7 +40,50 @@ public class FFmpegService implements GetMediaInfoInterface, ConvertInterface{
 
     @Override
     public void convertVideo(VideoJob job) {
+        //TODO throw descriptive errors.
 
+        String input = job.getInputFileName();
+        String output = job.getOutputFileName();
+
+        int audioChannels = job.getAudioAttributes().getChannels();
+        String audioCodec = job.getAudioAttributes().getCodecName();
+        int audioSampleRate = job.getAudioAttributes().getSampleRate();
+        int audioBitRate = job.getAudioAttributes().getBitrate();
+
+        String videoCodec = job.getVideoAttributes().getCodecName();
+        long videoBitrate = job.getVideoAttributes().getBitrate();
+        int frameRate = job.getVideoAttributes().getFps();
+        int width = job.getVideoAttributes().getWidth();
+        int height = job.getVideoAttributes().getHeight();
+
+        FFmpegBuilder builder = new FFmpegBuilder()
+
+                .setInput(input)
+                .overrideOutputFiles(true) // Override the output if it exists
+
+                .addOutput(output)   // Filename for the destination
+//                .setFormat("mp4")        // Format is inferred from filename, or can be set
+//                .setTargetSize(250_000)  // Target size in KB
+
+                .disableSubtitle()       // No subtiles
+
+                .setAudioChannels(audioChannels)
+                .setAudioCodec(audioCodec)
+                .setAudioSampleRate(audioSampleRate)
+                .setAudioBitRate(audioBitRate)
+
+                .setVideoCodec(videoCodec)
+                .setVideoFrameRate(frameRate, 1)
+                .setVideoBitRate(videoBitrate)
+                .setVideoResolution(width, height)
+
+                .setStrict(FFmpegBuilder.Strict.EXPERIMENTAL) // Allow FFmpeg to use experimental specs
+                .done();
+
+        FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
+
+        // Run a one-pass encode
+        executor.createJob(builder).run();
     }
 
     @Override
