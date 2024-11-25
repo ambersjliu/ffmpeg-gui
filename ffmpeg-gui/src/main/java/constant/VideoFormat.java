c0059f65ecbfba1bde78530d95877e654527f943
notming11 <yinming.chan@gmail.com>
2024-11-25 14:24:24 -0500

    webm get the hell outta here

--- ffmpeg-gui/src/main/java/constant/VideoFormat.java
+++ ffmpeg-gui/src/main/java/constant/VideoFormat.java
@@ -1,5 +1,5 @@
 package constant;
 
 public class VideoFormat {
-    public static final String[] VIDEO_FORMATS = {"mp4", "mov", "avi", "webm", "mpeg", "m4v"};
+    public static final String[] VIDEO_FORMATS = {"mp4", "mov", "avi", "mpeg", "m4v"};
 }
