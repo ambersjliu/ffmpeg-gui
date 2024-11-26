d3eacc5450ce4e885c1b48855c9b5e5f47ae90f0
ambersjliu <90348951+ambersjliu@users.noreply.github.com>
2024-11-25 22:43:56 -0500

    Added free scaling and default format

--- ffmpeg-gui/src/main/java/constant/VideoFormat.java
+++ ffmpeg-gui/src/main/java/constant/VideoFormat.java
@@ -2,4 +2,5 @@ package constant;
 
 public class VideoFormat {
     public static final String[] VIDEO_FORMATS = {"mp4", "mov", "avi", "webm", "mpeg", "m4v", "gif"};
+    public static final String DEFAULT_FORMAT = "mp4";
 }
