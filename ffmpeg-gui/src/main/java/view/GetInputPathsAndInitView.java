347cd18971297fd0ad83650462ad35a38a54f596
ambersjliu <90348951+ambersjliu@users.noreply.github.com>
2024-11-30 21:22:49 -0500

    Finished getinputpathsandinit view checkstyle

--- ffmpeg-gui/src/main/java/view/GetInputPathsAndInitView.java
+++ ffmpeg-gui/src/main/java/view/GetInputPathsAndInitView.java
@@ -1,14 +1,7 @@
 package view;
 
-import interface_adapter.get_paths_and_init.GetInputPathsAndInitController;
-import interface_adapter.get_paths_and_init.GetInputPathsAndInitState;
-import interface_adapter.get_paths_and_init.GetInputPathsAndInitViewModel;
-import lombok.Setter;
-
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.Component;
+import java.awt.Desktop;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.beans.PropertyChangeEvent;
@@ -17,8 +10,20 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-public class GetInputPathsAndInitView extends JPanel implements ActionListener, PropertyChangeListener {
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
+import interface_adapter.get_paths_and_init.GetInputPathsAndInitController;
+import interface_adapter.get_paths_and_init.GetInputPathsAndInitState;
+import interface_adapter.get_paths_and_init.GetInputPathsAndInitViewModel;
+import lombok.Setter;
+
+/**
+ * View for getting the paths to the FFmpeg and FFprobe binaries, then initializing.
+ */
+public class GetInputPathsAndInitView extends JPanel implements PropertyChangeListener {
 
     private final GetInputPathsAndInitViewModel getInputPathsAndInitViewModel;
     @Setter
@@ -39,31 +44,31 @@ public class GetInputPathsAndInitView extends JPanel implements ActionListener, 
         this.getInputPathsAndInitViewModel = getInputPathsAndInitViewModel;
         this.getInputPathsAndInitViewModel.addPropertyChangeListener(this);
 
-        //Title at the top.
+        // Title at the top.
         final JLabel title = new JLabel(GetInputPathsAndInitViewModel.TITLE_LABEL);
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        //Instructions to install FFMPEG, with a provided clickable hyperlink
-        JLabel instructionLabel = new JLabel(GetInputPathsAndInitViewModel.INTRUCTION_LABEL);
+        // Instructions to install FFMPEG, with a provided clickable hyperlink
+        final JLabel instructionLabel = new JLabel(GetInputPathsAndInitViewModel.INTRUCTION_LABEL);
         hyperlinkLabel.setText(GetInputPathsAndInitViewModel.HYPERLINK_LABEL);
 
-        //Directs the user to locate FFMPEG from the computer, with a button that will launch a file explorer dialog
-        JLabel ffmpegLabel = new JLabel(GetInputPathsAndInitViewModel.FFMPEG_LABEL);
+        // Directs the user to locate FFMPEG from the computer, with a button that will launch a file explorer dialog
+        final JLabel ffmpegLabel = new JLabel(GetInputPathsAndInitViewModel.FFMPEG_LABEL);
         ffmpegButton = new JButton(GetInputPathsAndInitViewModel.BROWSE_TEXT);
         final LabelButtonPanel ffmpegBrowse = new LabelButtonPanel(ffmpegLabel, ffmpegButton);
 
-        //Directs the user to locate FFPROBE from the computer, with a button that will launch a file explorer dialog
-        JLabel ffprobeLabel = new JLabel(GetInputPathsAndInitViewModel.FFPROBE_LABEL);
+        // Directs the user to locate FFPROBE from the computer, with a button that will launch a file explorer dialog
+        final JLabel ffprobeLabel = new JLabel(GetInputPathsAndInitViewModel.FFPROBE_LABEL);
         ffprobeButton = new JButton(GetInputPathsAndInitViewModel.BROWSE_TEXT);
         final LabelButtonPanel ffprobeBrowse = new LabelButtonPanel(ffprobeLabel, ffprobeButton);
 
-        //Once the files are selected, the next button allows the user to move on with the program.
+        // Once the files are selected, the next button allows the user to move on with the program.
         final JButton nextButton = new JButton(GetInputPathsAndInitViewModel.NEXT_TEXT);
         nextButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         ffmpegButton.addActionListener(
-                e -> {
-                    if (e.getSource() == ffmpegButton) {
+                evt -> {
+                    if (evt.getSource() == ffmpegButton) {
                         final GetInputPathsAndInitState currentState = getInputPathsAndInitViewModel.getState();
                         currentState.setFfmpegPath(fileChooserDialog.takeFilePath());
                         getInputPathsAndInitViewModel.firePropertyChanged();
@@ -72,8 +77,8 @@ public class GetInputPathsAndInitView extends JPanel implements ActionListener, 
         );
 
         ffprobeButton.addActionListener(
-                e -> {
-                    if (e.getSource() == ffprobeButton) {
+                evt -> {
+                    if (evt.getSource() == ffprobeButton) {
                         final GetInputPathsAndInitState currentState = getInputPathsAndInitViewModel.getState();
                         currentState.setFfprobePath(fileChooserDialog.takeFilePath());
                         getInputPathsAndInitViewModel.firePropertyChanged();
@@ -87,39 +92,37 @@ public class GetInputPathsAndInitView extends JPanel implements ActionListener, 
                     public void mouseClicked(MouseEvent e) {
                         try {
                             Desktop.getDesktop().browse(new URI("https://www.ffmpeg.org/download.html#build-windows"));
-                        } catch (IOException | URISyntaxException ex) {
+                        }
+                        catch (IOException | URISyntaxException ex) {
                             throw new RuntimeException(ex);
                         }
                     }
-                });
+                }
+        );
 
         nextButton.addActionListener(
-                e -> {
+                evt -> {
                     final GetInputPathsAndInitState currentState = getInputPathsAndInitViewModel.getState();
                     getInputPathsAndInitController.execute(currentState.getFfmpegPath(), currentState.getFfprobePath());
                 }
         );
 
-        JPanel intructionPanel = new JPanel();
-        intructionPanel.add(instructionLabel);
-        intructionPanel.add(hyperlinkLabel);
+        final JPanel instructionPanel = new JPanel();
+        instructionPanel.add(instructionLabel);
+        instructionPanel.add(hyperlinkLabel);
 
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
         pathErrorfield.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         this.add(title);
-        this.add(intructionPanel);
+        this.add(instructionPanel);
         this.add(ffmpegBrowse);
         this.add(ffmpegPathField);
         this.add(ffprobeBrowse);
         this.add(ffprobePathField);
         this.add(pathErrorfield);
         this.add(nextButton);
-    }
-
-    public void actionPerformed(ActionEvent evt) {
-
     }
 
     @Override
